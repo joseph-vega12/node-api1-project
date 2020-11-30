c73a54f61bb4032cc23d4dece002122b262c810a
joseph-vega12 <57374454+joseph-vega12@users.noreply.github.com>
2020-11-30 14:00:44 -0800

    added get /api/users and dummy data

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const server = express();
 
+const shortid = require('shortid'); 
+
+//fake dummy user data'
+let users = {
+    id: shortid.generate(),
+    name: "Jane Doe", 
+    bio: "Not Tarzan's Wife, another Jane", 
+}
+
+const Users = {
+    getAll() {
+        return users;
+    },
+}
+
+server.get('/api/users', (req, res) => {
+    const users = Users.getAll();
+    res.status(200).json(users);
+})
 
 server.use('*', (req, res) => {
     res.status(404).json({message: "not found"});
