bf17515c111d29b0fd4f6280f989ffabf7784a2f
joseph-vega12 <57374454+joseph-vega12@users.noreply.github.com>
2020-11-30 14:39:03 -0800

    added the ability to add new user

--- server.js
+++ server.js
@@ -1,28 +1,40 @@
 const express = require('express');
 const server = express();
 
-const shortid = require('shortid'); 
+const shortid = require('shortid');
+server.use(express.json());
 
 //fake dummy user data'
-let users = {
-    id: shortid.generate(),
-    name: "Jane Doe", 
-    bio: "Not Tarzan's Wife, another Jane", 
-}
-
+let users = [];
 const Users = {
+    createNew(user) {
+        const newUser = { id: shortid.generate(), ...user };
+        users.push(newUser);
+        return newUser;
+    },
     getAll() {
         return users;
     },
 }
 
+server.post('/api/users', (req, res) => {
+    const userFromClient = req.body;
+
+    if (!userFromClient.name || !userFromClient.bio) {
+        res.status(400).json({ message: 'name and bio are required!' })
+    } else {
+        const newlyCreatedUser = Users.createNew(userFromClient);
+        res.status(201).json(newlyCreatedUser);
+    }
+})
 server.get('/api/users', (req, res) => {
     const users = Users.getAll();
     res.status(200).json(users);
 })
 
+
 server.use('*', (req, res) => {
-    res.status(404).json({message: "not found"});
+    res.status(404).json({ message: "not found" });
 })
 
 server.listen(5000, () => {
