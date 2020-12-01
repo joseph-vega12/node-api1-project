f163b937ad05de4143c1965b92b92f9a05520648
joseph-vega12 <57374454+joseph-vega12@users.noreply.github.com>
2020-11-30 21:17:50 -0800

    added deleted functionality

--- server.js
+++ server.js
@@ -8,19 +8,56 @@ server.use(express.json());
 let users = [];
 
 const Users = {
+    getAll() {
+        return users;
+    },
+    getById(id) {
+        return users.find(u => u.id === id);
+    },
     createNew(user) {
         const newUser = { id: shortid.generate(), ...user };
         users.push(newUser);
         return newUser;
     },
-    getAll() {
+    delete(id) {
+        const user = users.find(u => u.id === id)
+        if(user){
+            users = users.filter(u => u.id !== id)
+        }
         return users;
     },
-    getById(id){
-        return users.find(user => user.id === id);
+    update(id, changes) {
+        const user = users.find(u => u.id === id)
+        if (!user) {
+            return null
+        } else {
+            const updatedUser = { id, ...changes }
+            users = users.map(d => {
+                if (d.id === id) return { id, ...changes }
+                return d
+            })
+            return { id, ...changes }
+        }
     }
 }
 
+server.get('/api/users', (req, res) => {
+    const users = Users.getAll();
+    if (!users) {
+        res.status(500).json({ errorMessage: "The users information could not be retrieved." })
+    } else {
+        res.status(200).json(users);
+    }
+})
+server.get('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    const user = Users.getById(id);
+    if (user) {
+        res.status(200).json(user);
+    } else {
+        res.status(404).json({ message: "The user with the specified ID does not exist." });
+    }
+})
 server.post('/api/users', (req, res) => {
     const userFromClient = req.body;
 
@@ -31,21 +68,23 @@ server.post('/api/users', (req, res) => {
         res.status(201).json(newlyCreatedUser);
     }
 })
-server.get('/api/users', (req, res) => {
-    const users = Users.getAll();
-    if(!users){
-        res.status(500).json({errorMessage: "The users information could not be retrieved."})
-    } else {
-        res.status(200).json(users);
-    }
-})
-server.get('/api/users/:id', (req, res) => {
+server.delete('/api/users/:id', (req, res) => {
     const { id } = req.params;
-    const user = Users.getById(id);
-    if(user){
-        res.status(200).json(user);
+    const deleted = Users.delete(id);
+    if(deleted){
+        res.status(200).json(deleted);
     } else {
-        res.status(404).json({ message: "The user with the specified ID does not exist." });
+        res.status(404).json({message: 'id not found with ' + id});
+    }
+});
+server.put('/api/users/:id', (req, res) => {
+    const changes = req.body;
+    const { id } = req.params;
+    const updatedUser = Users.update(id, changes);
+    if (updatedUser) {
+        res.status(200).json(updatedUser);
+    } else {
+        res.status(404).json({ message: 'user not found with id ' + id })
     }
 })
 
